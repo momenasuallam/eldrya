5f5b52cb8e0c92b75d6b7e3c09929075b680a370
momenasuallam <59420500+momenasuallam@users.noreply.github.com>
2021-10-27 20:05:22 +0500

    working

--- js/myjs.js
+++ js/myjs.js
@@ -1,16 +1,5 @@
-$(".toggle-password").click(function () {
-  $(this).toggleClass("fa-eye fa-eye-slash");
-  var input = $($(this).attr("toggle"));
-  if (input.attr("type") == "password") {
-    input.attr("type", "text");
-  } else {
-    input.attr("type", "password");
-  }
-});
-$(document).ready(function () {
-  $(".sidebarCollapse").on("click", function () {
-    $("#sidebar").toggleClass("active");
-    $(".diswhenhidesb button").toggleClass("hellosidebtn");
-    $("#content").toggleClass("bgactive");
-  });
-});
+// var jQliList = $("ul.steps li");
+// jQliList.on("click", function () {
+//   jQliList.removeClass("active");
+//   $(this).addClass("active");
+// });
